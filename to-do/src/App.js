eb2d4d7c296e9085c35accf2ec5eeb5b480e211c
Murat Bukarbaev <murat.bukarbaev@gmail.com>
2024-07-30 11:48:34 +0600

    test2

---
+++ to-do/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MainPage from './pages/mainPage/MainPage';
+import './App.scss';
+import './index.scss';
+
+function App() {
+    return (
+        <div className="App">
+            <MainPage />
+        </div>
+    );
+}
+
+export default App;
